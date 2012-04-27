3d24de594cffa3598e3d2660728a16e08f568ca5
Brent Wooden <brent.wooden@gmail.com>
2012-04-27 05:05:04 -0700

    CSS file

--- javascripts/presenting_json.js
+++ javascripts/presenting_json.js
@@ -2,6 +2,7 @@ $(function() {
   (function($){  
 function PresentingJson(element, options){
   var self = $(element);
+    var html = "";
   
   function image_collapsed(){
     return '<img src="images/collapsed.gif" />';
@@ -20,11 +21,11 @@ function PresentingJson(element, options){
   }
 
   function add_wrapped_key(attribute){
-    html += wrap_key(attribute) + ' : ';
+    html += wrap_key(attribute) + ' <span class="json_colon">:</span> ';
   }
 
   function add_line_break(){
-    html += ',<br />';
+    html += '<span class="json_comma">,</span><br />';
   }
 
   function process_string_node(node, attribute, keyed){
@@ -32,23 +33,23 @@ function PresentingJson(element, options){
       add_wrapped_key(attribute);
     }
   
-    html += '"' + node[attribute] + '"';    
+    html += '<span class="json_string">"' + node[attribute] + '"</span>';    
   }
 
-  function process_non_quoted_type(node, attribute, keyed){
+  function process_number_node(node, attribute, keyed){
     if(keyed){
       add_wrapped_key(attribute);
     }
   
-    html += node[attribute]; 
-  }
-
-  function process_number_node(node, attribute, keyed){
-    process_non_quoted_type(node, attribute, keyed);
+    html += '<span class="json_number">' + node[attribute] + '</span>';    
   }
 
   function process_boolean_node(node, attribute, keyed){
-    process_non_quoted_type(node, attribute, keyed);
+    if(keyed){
+      add_wrapped_key(attribute);
+    }
+  
+    html += '<span class="json_boolean">' + node[attribute] + '</span>';    
   }
 
   function process_null_node(node, attribute, keyed){
@@ -56,7 +57,7 @@ function PresentingJson(element, options){
       add_wrapped_key(attribute);      
     }
   
-    html += 'null';
+    html += '<span class="json_null">null</span>';
   }
 
   function process_object_node(node, attribute, keyed){
@@ -97,7 +98,6 @@ function PresentingJson(element, options){
     html += '</div> }';    
   }
 
-  var html = "";
   function process_node(node){
     if(node instanceof Array){
       process_array_node(node);
@@ -122,17 +122,16 @@ function PresentingJson(element, options){
     })
   }
 
-   function initialize(element){
-     parsed_json = $.parseJSON(self.html());     
-   
-     process_node(parsed_json);
+   function initialize(element){   
+     html += '<span class="presenting_json_wrap">';
+     process_node($.parseJSON(self.html()));
+     html += '</span>'
      self.html(html);
      bind_events();
    }
    
   initialize();   
  }
-
  
    $.fn.presentJson = function(options) {
      var options = options || {};
